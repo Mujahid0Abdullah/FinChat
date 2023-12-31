3a8371c1859a73904a09a0e6145c34357d73401b
Mujahid alabdullah <149869699+Ogrenci123@users.noreply.github.com>
2023-12-31 16:32:24 +0300

    hata bulması

--- index.js
+++ index.js
@@ -75,6 +75,8 @@ app.get("/", (req, res) => {
     console.log("login");
 
     const htmlPath = path.resolve('views', 'login.html');
+    console.log(htmlPath);
+
     res.sendFile(htmlPath);
 });
 
