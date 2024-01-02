1dc2c92829849d5f9649f082b42d54a300da8d72
Mujahid alabdullah <149869699+Ogrenci123@users.noreply.github.com>
2024-01-02 16:11:21 +0300

    get img

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,62 @@
 import Express from "express";
 import { authorize } from '../proxy.js';
+import path from "path";
+
 
 import { getUser, updateUser } from "../controllers/user.js"; //.js
 
 const router = Express.Router()
+const publicPath = path.resolve('public');
+
+console.log(publicPath);
 
 
 router.get("/", getUser)
 
 router.put("/", authorize, updateUser)
+
+
+/*
+const storage = multer.diskStorage({
+
+    destination: function (req, file, cb) {
+        cb(null, publicPath); // Dosyanın kaydedileceği yol
+
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // Dosya adı ve uzantısı
+        console.log(Date.now() + path.extname(file.originalname))
+
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
+router.post("/", authorize, upload.single('img'), (req, res) => {
+
+    console.log(req.body);
+    const file = req.file;
+    const imagePath = `${publicPath}/${file.filename}`;
+    const descValue = req.body.desc;
+    const userInfo = req.userInfo; // Yetkilendirme adımında doğrulanan kullanıcı bilgileri
+
+    const q = "INSERT INTO posts (`desc`, `img`, `userId`, `createdAt`) VALUES (?)";
+    const values = [
+        descValue,
+        imagePath,
+        userInfo.id,
+        moment(Date.now()).format("YYYY-MM-DD HH:mm:ss")
+    ];
+
+    // Veritabanına eklemek için isteği işle
+    db.query(q, [values], (err, data) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+        return res.status(200).json("post eklendi");
+    });
+})*/
+
 
 export default router
