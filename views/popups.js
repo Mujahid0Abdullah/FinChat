655d4d64e8b380d36980b7e68afd2f2deaef898c
Ogrenci123 <firatprojeler@hotmail.com>
2024-01-03 16:28:57 +0300

    sondurum

--- views/popups.js
+++ views/popups.js
@@ -1,3 +1,4 @@
+var closePopupTimeout;
 
 function togglePopup(event) {
     event.stopPropagation();
@@ -24,54 +25,23 @@ function openDynamicPopup() {
     dynamicPopup.style.display = "block";
 }
 
+
 function closeDynamicPopup() {
     var dynamicPopup = document.getElementById("dynamic-popup");
     dynamicPopup.style.display = "none";
 }
 
 function openProfilePage() {
-    window.location.href = "./profile";
+    window.location.href = "profile.html";
 }
 
 function cancelClosePopup() {
-    clearTimeout(this.closePopupTimeout);
+    clearTimeout(closePopupTimeout);
 }
 
 function closePopupWithDelay() {
-    this.closePopupTimeout = setTimeout(() => {
+    closePopupTimeout = setTimeout(function () {
         var popup = document.getElementById("popup");
         popup.style.display = "none";
-<<<<<<< HEAD
-    }
-
-    openDynamicPopup() {
-        console.log("Opening dynamic popup");
-        var dynamicPopup = document.getElementById("dynamic-popup");
-        dynamicPopup.style.display = "block";
-    }
-
-    closeDynamicPopup() {
-        var dynamicPopup = document.getElementById("dynamic-popup");
-        dynamicPopup.style.display = "none";
-    }
-
-    openProfilePage() {
-        window.location.href = "profile.html";
-    }
-
-    cancelClosePopup() {
-        clearTimeout(this.closePopupTimeout);
-    }
-
-    closePopupWithDelay() {
-        this.closePopupTimeout = setTimeout(() => {
-            var popup = document.getElementById("popup");
-            popup.style.display = "none";
-        }, 100); // 1 saniye gecikme
-    }
-
-}
-=======
-    }, 100); // 1 saniye gecikme
-}
->>>>>>> 57171832fec011b12f33a49a9b5c584f09ad89e9
+    }, 100); // 1 saniye gecikme
+};
