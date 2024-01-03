0524fb6be57fb9ec1aa4c4f71e9df593e21d58f4
Ogrenci123 <firatprojeler@hotmail.com>
2024-01-03 12:20:12 +0300

    singlatone

--- index.js
+++ index.js
@@ -150,6 +150,15 @@ app.get("/anasayfaJs", (req, res) => {
 });
 
 
+app.get("/popupJs", (req, res) => {
+    console.log("js");
+
+    const htmlPath = path.resolve('views', 'popup.js');
+    console.log(htmlPath);
+
+    res.sendFile(htmlPath);
+});
+
 
 
 app.get("/upload", (req, res) => {
