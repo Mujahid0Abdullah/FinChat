74c03dee146df72e2269452a77b04353fbe85125
Mujahid alabdullah <149869699+Ogrenci123@users.noreply.github.com>
2024-01-03 12:38:45 +0300

    k

--- controllers/post.js
+++ controllers/post.js
@@ -17,40 +17,8 @@ export const getPosts = (req, res) => {
 };
 
 
-export const getmyPosts = (req, res) => {
-    const q = "SELECT p.*, u.id AS userId, name, profilePic FROM posts p JOIN users u ON (u.id = p.userId) WHERE u.id=? ORDER BY p.createdAt DESC";
-    db.query(q, [req.userInfo.id], (err, data) => {
-        if (err) {
-            return res.status(500).json(err);
-        }
-        return res.status(200).json(data);
-    });
-};
 
-export const getpost = (req, res) => {
-    // Burada post ID'sini almak istediğiniz postun detaylarını alın
-    // postDetails değişkeni örnek olarak bir postun detaylarını içeriyor olsun
 
-    const q = "SELECT p.*, u.id AS userId, name, profilePic FROM posts p JOIN users u ON (u.id = p.userId) WHERE p.id=? ORDER BY p.createdAt DESC";
-    console.log("q" + req.query);
-    // console.log("p" + req.param);
-
-    const id = req.query.postId;
-
-    db.query(q, [id], (err, data) => {
-        if (err) { return res.status(500).json(err); }
-
-        return res.status(200).json(data);
-    });
-    /*
-    const postDetails = {
-        id: req.params.postID,
-        title: 'Post Title',
-        content: 'Post Content',
-        // Diğer post özellikleri...
-    };*/
-    // Örnek bir HTML dosyasını göndermek istediğiniz sayfanın dizinini belirtin
-};
 
 export const addPost = (req, res) => {
 
@@ -94,62 +62,34 @@ export const deletePost = (req, res) => {
 };
 
 
-/*
-export const addPost = (req, res) => {
-    console.log(req.body);
-    const token = req.cookies.accessToken;
-    const file = req.file;
-    const imagePath = `/uploads/${file.filename}`;
-    if (!token) return res.status(401).json("Not logged in")
-
-    jwt.verify(token, "SecretKey", (err, userInfo) => {
-        if (err) return res.status(403).json("Token is not valid");
 
 
-        const q = "INSERT INTO posts (`desc`, `img`, `userId`, `createdAt`) VALUES (?)";
+export const getmyPosts = (req, res) => {
+    const q = "SELECT p.*, u.id AS userId, name, profilePic FROM posts p JOIN users u ON (u.id = p.userId) WHERE u.id=? ORDER BY p.createdAt DESC";
+    db.query(q, [req.userInfo.id], (err, data) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+        return res.status(200).json(data);
+    });
+};
 
-        const descValue = req.body.des;
-        console.log(descValue);
-        const values = [
-            descValue,
-            imagePath,
-            userInfo.id,
+export const getpost = (req, res) => {
+    // Burada post ID'sini almak istediğiniz postun detaylarını alın
+    // postDetails değişkeni örnek olarak bir postun detaylarını içeriyor olsun
 
-            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss")
-        ];
+    const q = "SELECT p.*, u.id AS userId, name, profilePic FROM posts p JOIN users u ON (u.id = p.userId) WHERE p.id=? ORDER BY p.createdAt DESC";
+    console.log("q" + req.query);
+    // console.log("p" + req.param);
 
-        db.query(q, [values], (err, data) => {
-            if (err) return res.status(500).json(err);
-            return res.status(200).json("post eklendi");//builder pattern
-        });
-    })
-}*/
-/*
-export const addPost = (req, res) => {
-    const token = req.cookies.accessToken;
+    const id = req.query.postId;
 
-    if (!token) return res.status(401).json("Not logged in")
+    db.query(q, [id], (err, data) => {
+        if (err) { return res.status(500).json(err); }
 
-    jwt.verify(token, "SecretKey", (err, userInfo) => {
-        if (err) return res.status(403).json("Token is not valid");
+        return res.status(200).json(data);
+    });
+};
 
 
-        const q = "INSERT INTO posts (`desc`, `img`, `userId`, `createdAt`) VALUES (?)";
 
-        const descValue = req.body.des;
-        console.log(descValue);
-        const values = [
-            descValue,
-            req.body.img,
-            userInfo.id,
-
-            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss")
-        ];
-
-        db.query(q, [values], (err, data) => {
-            if (err) return res.status(500).json(err);
-            return res.status(200).json("post eklendi");//builder pattern
-        });
-    })
-}
-    */
