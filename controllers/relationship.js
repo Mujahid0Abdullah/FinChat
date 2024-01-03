2db0d2ba0d712b97cb52d0d0957cae065b99956e
Mujahid alabdullah <149869699+Ogrenci123@users.noreply.github.com>
2024-01-03 23:06:49 +0300

    fff

--- controllers/relationship.js
+++ controllers/relationship.js
@@ -10,8 +10,9 @@ export const getRelationships = (req, res) => {
 
     db.query(q, [req.query.followedUserId], (err, data) => {
         if (err) return res.status(500).json(err);
-        return res.status(200).json(data.map(relationship => relationship.followerUserId));
+        return res.status(200).json(data);
     });
+    //.map(relationship => relationship.followerUserId)
 }
 
 export const addRelationship = (req, res) => {
