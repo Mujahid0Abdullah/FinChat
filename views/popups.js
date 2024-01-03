25095fb63743f0e67d7b24432a9949c459a3c039
Mujahid alabdullah <149869699+Ogrenci123@users.noreply.github.com>
2024-01-03 12:46:47 +0300

    popup

--- views/popups.js
+++ views/popups.js
@@ -1,50 +1,45 @@
-class PopupManager {
-    constructor() {
-        this.closePopupTimeout = null;
-    }
 
-    togglePopup(event) {
+function togglePopup(event) {
+    event.stopPropagation();
+    var popup = document.getElementById("popup");
+    popup.style.display = popup.style.display === "none" ? "block" : "none";
+}
+
+function openPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+
+function closePopup(event) {
+    if (event) {
         event.stopPropagation();
-        var popup = document.getElementById("popup");
-        popup.style.display = popup.style.display === "none" ? "block" : "none";
     }
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
 
-    openPopup() {
-        var popup = document.getElementById("popup");
-        popup.style.display = "block";
-    }
+function openDynamicPopup() {
+    console.log("Opening dynamic popup");
+    var dynamicPopup = document.getElementById("dynamic-popup");
+    dynamicPopup.style.display = "block";
+}
 
-    closePopup(event) {
-        if (event) {
-            event.stopPropagation();
-        }
+function closeDynamicPopup() {
+    var dynamicPopup = document.getElementById("dynamic-popup");
+    dynamicPopup.style.display = "none";
+}
+
+function openProfilePage() {
+    window.location.href = "profile.html";
+}
+
+function cancelClosePopup() {
+    clearTimeout(this.closePopupTimeout);
+}
+
+function closePopupWithDelay() {
+    this.closePopupTimeout = setTimeout(() => {
         var popup = document.getElementById("popup");
         popup.style.display = "none";
-    }
-
-    openDynamicPopup() {
-        console.log("Opening dynamic popup");
-        var dynamicPopup = document.getElementById("dynamic-popup");
-        dynamicPopup.style.display = "block";
-    }
-
-    closeDynamicPopup() {
-        var dynamicPopup = document.getElementById("dynamic-popup");
-        dynamicPopup.style.display = "none";
-    }
-
-    openProfilePage() {
-        window.location.href = "profile.html";
-    }
-
-    cancelClosePopup() {
-        clearTimeout(this.closePopupTimeout);
-    }
-
-    closePopupWithDelay() {
-        this.closePopupTimeout = setTimeout(() => {
-            var popup = document.getElementById("popup");
-            popup.style.display = "none";
-        }, 100); // 1 saniye gecikme
-    }
+    }, 100); // 1 saniye gecikme
 }
