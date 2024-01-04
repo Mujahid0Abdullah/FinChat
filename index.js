f7caf138c94cb0d28ea229153e102dff785c55e3
Mujahid alabdullah <149869699+Ogrenci123@users.noreply.github.com>
2024-01-04 17:12:34 +0300

    static

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ import relationshipRoutes from "./routes/relationships.js"
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("views"))
 
 
 app.use((req, res, next) => {
