0ac3d8c7ee5e1f654087de8c56dd943f036f6c3f
Mujahid alabdullah <94225087+Mujahid0Abdullah@users.noreply.github.com>
2024-01-06 14:31:55 +0300

    sql

--- connect.js
+++ connect.js
@@ -1,8 +1,16 @@
 import mysql from "mysql"
 
+
+export const db = mysql.createConnection({
+    host: "byg6pj4yq5rqua2wemyy-mysql.services.clever-cloud.com",
+    user: "u99cuyvzdmjtwugr",
+    password: "UBuQ4H30F1qnpCtLkOV5"
+    , database: "byg6pj4yq5rqua2wemyy"
+})
+/*
 export const db = mysql.createConnection({
     host: "sql11.freesqldatabase.com",
     user: "sql11673798",
     password: "hjU4wxvFs2"
     , database: "sql11673798"
-})
+})*/
