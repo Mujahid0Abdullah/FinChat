4bddfc071e46678e601d9ad836cdcca79a846a1a
Mujahid alabdullah <94225087+Mujahid0Abdullah@users.noreply.github.com>
2024-01-10 17:40:15 +0300

    d

--- connect.js
+++ connect.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const db = mysql.createConnection({
     host: "byg6pj4yq5rqua2wemyy-mysql.services.clever-cloud.com",
     user: "u99cuyvzdmjtwugr" ,
-    password: process.env.DBPASSWORD
+    password: "UBuQ4H30F1qnpCtLkOV5"
     , database: "byg6pj4yq5rqua2wemyy"
 })
 /*
@@ -15,5 +15,5 @@ export const db = mysql.createConnection({
     password: "hjU4wxvFs2"
     , database: "sql11673798"
 process.env.OPENKEY
-UBuQ4H30F1qnpCtLkOV5
+
 })*/
